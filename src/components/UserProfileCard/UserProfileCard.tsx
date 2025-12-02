5a78a65adf4d3b875347a0a8773064813f985f90
Josh Fisher <jfisherd@gmail.com>
2025-12-02 16:13:04 -0500

    server runs using 'npm run dev', types/index.ts: populate with types, UserProfileCard.tsx: import props and frame export function

---
+++ src/components/UserProfileCard/UserProfileCard.tsx
@@ -0,0 +1,11 @@
+import { UserProfileCardProps } from "../../types";
+
+
+export const UserProfileCard = () => {
+    return (<>
+        <div>This is my div</div>
+
+    </>)
+}
+
+// export default UserProfileCard

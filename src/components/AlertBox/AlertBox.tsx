ca8e51e1f1dc1f0679849cafdc6f74d8c5bd1db1
Josh Fisher <jfisherd@gmail.com>
2025-12-03 17:59:40 -0500

    AlertBox.tsx import from types from index.ts

---
+++ src/components/AlertBox/AlertBox.tsx
@@ -0,0 +1,2 @@
+import type { AlertBoxProps } from "../../types";
+
--- src/components/UserProfileCard/UserProfileCard.tsx
+++ src/components/UserProfileCard/UserProfileCard.tsx
@@ -1,13 +1,17 @@
+// import React from "react"; // NEEDED?
+import type { User } from "../../types";
 import type { UserProfileCardProps } from "../../types";
 
 
 
 export const UserProfileCard = () => {
-    return (<>
-        <div>User: {}</div>
-        <div>Email: {}</div>
-        <div>Role: {}</div>
-    </>)
+    return (
+        <>
+            <div>User: { }</div>
+            <div>Email: { }</div>
+            <div>Role: { }</div>
+        </>
+    )
 }
 
 // export default UserProfileCard

3cb1675e17e0b13a73b9fd272b634e79c90458cb
Josh Fisher <jfisherd@gmail.com>
2025-12-03 18:29:20 -0500

    AlertBox.tsx, implement basic functionality

--- src/components/AlertBox/AlertBox.tsx
+++ src/components/AlertBox/AlertBox.tsx
@@ -1,2 +1,10 @@
 import type { AlertBoxProps } from "../../types";
 
+export const AlertBox = (props: AlertBoxProps) => {
+    return (
+        <>
+            <div>my alert box</div>
+            <div>Message: {props.message}</div>
+        </>
+    )
+}

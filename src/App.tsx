05196bbdb14f8b34e23f57b0e2f5156e455d0653
Josh Fisher <jfisherd@gmail.com>
2025-12-03 19:32:16 -0500

    All components display as expected, basic examples implemented

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,39 @@ import './App.css'
 
 import { AlertBox } from './components/AlertBox/AlertBox'
 import { UserProfileCard } from './components/UserProfileCard/UserProfileCard'
+import { ProductDisplay } from './components/ProductDisplay/ProductDisplay'
 
 function App() {
 
   return (
     <>
-    <h2>Alert Box</h2>
+      <h2>Alert Box</h2>
       <AlertBox
         type='success'
         message='my alert box message'
-        onClose={()=>alert('an alert')}
+        onClose={() => alert('an alert')}
       ></AlertBox>
       <br></br>
+      <h2>User Profile Card</h2>
+
       <UserProfileCard
-        user={{id:'123', name: 'myName', email: 'address@email.com', role: 'somebody'}}
+        user={{ id: '123', name: 'myName', email: 'address@email.com', role: 'somebody' }}
         showEmail={false}
         showRole={true}
       ></UserProfileCard>
+
+      <h2>Product Display</h2>
+      <ProductDisplay
+        product={{ id: 'A', name: 'Apple', price: 2, description: 'A pie apple', inStock: true }}
+        showDescription={true}
+        showStockStatus={true}
+      ></ProductDisplay>
+      <br></br>
+    <ProductDisplay
+      product={{ id:'B', name: 'Banana', price: 0.5, description: 'Only suitable for bread', inStock: true}}
+      showDescription={false}
+      // showStockStatus={false}
+      />
     </>
   )
 }
---
+++ src/components/ProductDisplay/ProductDisplay.tsx
@@ -0,0 +1,13 @@
+import type { ProductDisplayProps } from "../../types";
+
+export const ProductDisplay = (props: ProductDisplayProps) => {
+    return ( // In stock? asks if the stock info is available, then shows it if available
+        <>
+            <div>Product: {props.product.name}</div> {/*This is a comment inside the html fragment*/}
+            <div>Price: {props.product.price}</div>
+            <div>Description: {props.showDescription? props.product.description: 'Product description not available'}</div> 
+            <div>Availability: {props.showStockStatus? (props.product.inStock?'In stock':'Not in stock'): 'Stock status not available'}</div>
+        </>
+    )
+}
+
